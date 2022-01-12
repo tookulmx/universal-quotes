d21e07aef861e80c6ad87927eec020d16a3c7699
tookulmx <luisfragoso2.0@gmail.com>
2022-01-12 16:30:03 -0600

    New version

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ async function getQuotes() {
 // Tweet
 
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${'\"'}${quoteText.textContent}${'\"'} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank');
 }
 
